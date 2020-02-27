9a3103c881d0badac30f4d0b8d2d5f239d13bbf2
Maxwell Lasky <mlasky46@gmail.com>
2020-02-26 22:10:45 -0700

    Updates

---
+++ precache-manifest.bdabb11844d8be7da9826273bc6e71c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971efa8238f1542f1ac7bc4bfd948fbe",
+    "url": "/clapper-marketing/index.html"
+  },
+  {
+    "revision": "827628a8dfbb83221b5e",
+    "url": "/clapper-marketing/static/css/main.4ebee669.chunk.css"
+  },
+  {
+    "revision": "fbfea02c743649a089cf",
+    "url": "/clapper-marketing/static/js/2.2d6ef72e.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/clapper-marketing/static/js/2.2d6ef72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827628a8dfbb83221b5e",
+    "url": "/clapper-marketing/static/js/main.e52903ab.chunk.js"
+  },
+  {
+    "revision": "66dac79401f351c2d8d1",
+    "url": "/clapper-marketing/static/js/runtime-main.66d59012.js"
+  },
+  {
+    "revision": "2232b069d64656e9f11e2f07e9f1135f",
+    "url": "/clapper-marketing/static/media/demo.2232b069.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clapper-marketing/static/media/logo.5d5d9eef.svg"
+  }
+]);
