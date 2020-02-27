9d55a4d3502cfc5d41a49b73892a35b6731fb9a6
Maxwell Lasky <mlasky46@gmail.com>
2020-02-26 22:08:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
+
+import demo from "./demo.gif";
+import logo from "./logo.svg";
+import github from "./github.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div id="header-container">👏</div>
+
+      <div id="clap-container">
+        <motion.div transition={{ duration: 0.5 }} animate={{ scale: 0.5 }}>
+          <span id="inner-clap-container" role="img" aria-label="clap">
+            👏
+          </span>
+        </motion.div>
+      </div>
+
+      <div id="marketing-catch-phrase">
+        <h2> "Show 👏 me 👏 your 👏 tax 👏 returns" </h2>
+      </div>
+
+      <div id="demo-container">
+        <img src={demo} />
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +39,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <footer>
+        <div>© 2020 4thaboiz.com</div>
+
+        <img src={github} />
+
+        <div>Made with ❤️ on top of slack</div>
+      </footer>
     </div>
   );
 }
